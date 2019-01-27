27ddcc437a990eedfc0355ef964c59b54abd656a
KingDarBoja <mbojato@uninorte.edu.co>
2019-01-27 15:30:44 -0500

    Updated the project with angular material package and added nav bar

--- lib/app.component.dart
+++ lib/app.component.dart
@@ -1,10 +1,21 @@
 import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
 
 @Component(
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-)
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: [
+      'package:angular_components/app_layout/layout.scss.css',
+      './app.component.css'
+    ],
+    directives: [
+      MaterialButtonComponent,
+      MaterialIconComponent,
+    ],
+    providers: [
+      materialProviders
+    ])
+	
 class AppComponent {
-  var name = 'AngularDart';
+  var title = 'AngularDart';
 }
