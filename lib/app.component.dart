6e80e56a04d89dda4c32fcb07b50a49bb2efe7c6
KingDarBoja <mbojato@uninorte.edu.co>
2019-01-27 13:19:43 -0500

    Initial commit with code refactoring from original repo

---
+++ lib/app.component.dart
@@ -0,0 +1,10 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+)
+class AppComponent {
+  var name = 'AngularDart';
+}
